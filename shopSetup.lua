78559a6d494ac1f0cfde0b0c4b1aa160b8094926
NukeShot58 <adrian.filip@vp.pl>
2022-09-08 16:05:34 +0200

    .

--- shopSetup.lua
+++ shopSetup.lua
@@ -3,11 +3,11 @@ os.pullEvent = os.pullEventRaw
 if fs.exists('/Modules/ss_tweaks.lua') then
     fs.delete('/Modules/ss_tweaks.lua')
 end
-shell.execute('pastebin', 'get', 'UyrRb5hf', '/Modules/ss_tweaks.lua')
-if fs.exists('/Modules/ss_tweaks.lua') then
+shell.execute('wget', 'https://bit.ly/3x8xgvh', '/Modules/ss_tweaks.lua')
+if fs.exists('/shop/.shopCode.lua') then
     fs.delete('/shop/.shopCode.lua')
 end
-shell.execute('pastebin', 'get', 't707V832', '/shop/.shopCode.lua')
+shell.execute('wget', 'https://bit.ly/3eBzaOQ', '/shop/.shopCode.lua')
 term.clear()
 term.setCursorPos(1,1)
 textutils.slowPrint("Thank you for using D.A.R Shopping Software!")
